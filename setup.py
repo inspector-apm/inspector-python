14fb91f630dde956483c25e8f765122e22fb3c6d
Valerio Barbera <ilvalerione@gmail.com>
2022-11-15 15:06:09 +0100

    update version number

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='inspector-python',
     description='Connect your Python applications with Inspector.',
+    long_description='Inspector is a Code Execution Monitoring tool to help developers find out technical problem in their software before customers do.',
     version='0.1.4',
     extras_require=dict(tests=['pytest']),
     packages=find_packages(where="src"),
