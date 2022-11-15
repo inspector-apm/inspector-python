7acb9ca14db16b7333641fe3bd3dbcf4677c5098
Valerio Barbera <ilvalerione@gmail.com>
2022-11-15 14:52:37 +0100

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inspector-python',
-    version='0.1.2',
+    version='0.1.3',
     extras_require=dict(tests=['pytest']),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
